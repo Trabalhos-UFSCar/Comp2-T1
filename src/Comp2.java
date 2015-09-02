10d52157d43bede39bc6ed09ac3126f3b38b2351
Camilo Moreira <camilomoreira91@gmail.com>
2015-09-02 11:08:01 -0300

    Added FileChooser to the main class

--- src/Comp2.java
+++ src/Comp2.java
@@ -1,20 +1,35 @@
-package src;
-
 //import src.LAParser.ProgramaContext;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Comp2 {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        ANTLRInputStream input = new ANTLRInputStream(
-                new FileInputStream(args[0]));
+        JFileChooser fileChooser = new JFileChooser();
+        File selectedFile = null;
+        
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        int result = fileChooser.showOpenDialog(new JPanel());
+        if (result == JFileChooser.APPROVE_OPTION) {
+            selectedFile = fileChooser.getSelectedFile();
+        }
+        
+        System.out.println("Selected file: " + selectedFile.getName() 
+                + " " + selectedFile.length() + " Bytes");
+        
+        ANTLRInputStream input;
+        input = new ANTLRInputStream(
+                new FileInputStream(selectedFile));
 
         LALexer lexer = new LALexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+
         LAParser parser = new LAParser(tokens);
         parser.programa();
     }
