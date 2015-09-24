2cf5345839abe6690ab869a3080fcf3b93de82c7
João Paulo <smokeonline@gmail.com>
2015-09-24 11:13:56 -0300

    Gerando 2 códigos corretamente.

--- src/Comp2Visitor.java
+++ src/Comp2Visitor.java
@@ -1,4 +1,6 @@
 // Generated from .\src\LA.g4 by ANTLR 4.5.1
+import java.util.HashMap;
+import java.util.Map;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 
@@ -13,24 +15,29 @@ import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
 public class Comp2Visitor<T> extends LABaseVisitor<T> {
     
     public SaidaParser out;
+    Escopos escopos;
+    Map<String, String> tipo;
+    VerificadorDeTipos vdt;
+    //Essa variavel é usada para contornar a dificuldade que é decidir o tipo de uma variavel
+    //enquanto na regra mais_var. 
+    String tipoAtual = "";
+    
     public Comp2Visitor(SaidaParser out){
         this.out = out;
+        this.escopos = new Escopos();
+        this.tipo = new HashMap<>();
+        this.vdt = new VerificadorDeTipos(escopos);
+
+        // add os tipos em LA (key) e C (value)
+        tipo.put("inteiro", "int");
+        tipo.put("literal", "char");    // TODO: verificar como fazer literal em C
+        tipo.put("real", "float");
+        tipo.put("logico", "int");  // contorno para bool em C
     }
     
     public boolean regraVazia(ParserRuleContext ctx){
         return ctx.getText().equals("");
     } 
-    
-    public String tipo(String LATipo){
-        if(LATipo.equals("inteiro")){
-            return "int";
-        }else if(LATipo.equals("real")){
-            return "float";
-        }
-        else{
-            return "";
-        }
-    }
 
     /**
      * {@inheritDoc}
@@ -39,6 +46,8 @@ public class Comp2Visitor<T> extends LABaseVisitor<T> {
      * {@link #visitChildren} on {@code ctx}.</p>
      */
     @Override public T visitPrograma(LAParser.ProgramaContext ctx) { 
+        escopos.empilhar("global");
+        
         out.println("/* Arquivo gerado automaticamente */\n");
         out.println("#include <stdio.h>");
         out.println("#include <stdlib.h>\n");
@@ -53,6 +62,8 @@ public class Comp2Visitor<T> extends LABaseVisitor<T> {
         out.println("return 0;");
         out.identationLevel--;
         out.println("}");
+        
+        escopos.desempilhar();
         
         return null; 
     }
@@ -90,16 +101,22 @@ public class Comp2Visitor<T> extends LABaseVisitor<T> {
     }
     
     @Override public T visitVariavel(LAParser.VariavelContext ctx) { 
+        String nome = ctx.nome.getText();
+        tipoAtual = ctx.tipo().getText();
+        escopos.adicionarSimbolo(nome, tipoAtual);
+        
         String variavel = 
-         tipo(ctx.tipo().getText()) +
+         tipo.get(tipoAtual) +
          " " +
-         ctx.IDENT().getText() + 
+         nome + 
          visitDimensao(ctx.dimensao()) +
          visitMais_var(ctx.mais_var()) +
          ";"
         ;
         
         out.println(variavel);
+        
+        tipoAtual = "";
         
         return visitChildren(ctx); 
     }
@@ -139,11 +156,13 @@ public class Comp2Visitor<T> extends LABaseVisitor<T> {
     }
     
     @Override public T visitCorpo(LAParser.CorpoContext ctx) {
-        System.out.println("passei corpo");
+        escopos.empilhar("corpo");
         
         visitDeclaracoes_locais(ctx.declaracoes_locais());
         
         visitComandos(ctx.comandos());
+        
+        escopos.desempilhar();
         
         return visitChildren(ctx); 
     }
@@ -172,13 +191,26 @@ public class Comp2Visitor<T> extends LABaseVisitor<T> {
         return visitChildren(ctx); 
     }
     
-    @Override public T visitCmd(LAParser.CmdContext ctx) { 
+    @Override public T visitCmd(LAParser.CmdContext ctx) {
         String cmd = "";
         
         if(ctx.getStart().getText().equals("leia")){
-            cmd += "scanf(\"%d\",&" + visitIdentificador(ctx.identificador()) + ");";
+            String nome = (String) visitIdentificador(ctx.identificador());
+            String tipo = escopos.buscaSimbolo(nome).getTipo();
+        
+            if(tipo.equals("inteiro")){
+                cmd += "scanf(\"%d\",&" + nome + ");";
+            }else if(tipo.equals("real")){
+                cmd += "scanf(\"%f\",&" + nome + ");";
+            }
         }else if(ctx.getStart().getText().equals("escreva")){
-            cmd += "printf(\"%"+"\", " + visitExpressao(ctx.expressao()) + ");";
+            
+            System.out.println("Passei cmd");
+            if(vdt.verificaTipo(ctx.expressao()).equals("inteiro")){
+                cmd += "printf(\"%d"+"\", " + visitExpressao(ctx.expressao()) + ");";
+            }else if(vdt.verificaTipo(ctx.expressao()).equals("real")){
+                cmd += "printf(\"%f"+"\", " + visitExpressao(ctx.expressao()) + ");";
+            }
         }
         
         System.out.println(ctx.getText());
--- src/LABaseListener.java
+++ src/LABaseListener.java
@@ -1,7 +1,6 @@
-// Generated from .\src\LA.g4 by ANTLR 4.5.1
+// Generated from C:\Users\Jp\Documents\NetBeansProjects\Compiladores2\src\LA.g4 by ANTLR 4.5.1
 
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
